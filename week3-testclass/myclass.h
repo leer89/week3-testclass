43098fa13d252cfec82de5e8a94eaf892e8c8379
leer6 <leer6@AS315-D010>
2017-01-30 19:20:06 -0500

    week 3 test classes

---
+++ week3-testclass/myclass.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class myClass
+{
+public:
+	// member functions
+	void printNumbers(int x, int y);
+	void addNumbers(int x, int y);
+};
--- week3-testclass/week3-testclass.cpp
+++ week3-testclass/week3-testclass.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
-
+#include "myclass.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int x = 3;
+	int y = 4;
+	myClass test;
+
+	test.printNumbers(x, y);
+	test.addNumbers(x, y);
     return 0;
 }
 
+void myClass::printNumbers(int x, int y)
+{
+	cout << "Integer 1: " << x << endl;
+	cout << "Integer 2: " << y << endl;
+};
+
+void myClass::addNumbers(int x, int y)
+{
+	#include<iostream>
+	using namespace std;
+
+	int localSum = x + y;
+	cout << "The sum of the two numbers is: " << localSum << endl;
+};
